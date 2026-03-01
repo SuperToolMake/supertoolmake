62d3546ca73cd2e66f3756bf03c416250a6cdb50
mohagan9 <bournonville_deisel@aleeas.com>
2026-03-01 20:41:25 +0000

    fix: update redirect test expectations for Koa 3

--- packages/server/src/api/routes/tests/static.spec.ts
+++ packages/server/src/api/routes/tests/static.spec.ts
@@ -151,16 +151,14 @@ describe("/static", () => {
     it("should move permanently from base call (public call)", async () => {
       const res = await request.get(`/`)
       expect(res.status).toEqual(301)
-      expect(res.text).toEqual(
-        `Redirecting to <a href="/builder">/builder</a>.`
-      )
+      expect(res.text).toEqual(`Redirecting to /builder.`)
     })
 
     it("should not error when trying to get 'apple-touch-icon.png' (public call)", async () => {
       const res = await request.get(`/apple-touch-icon.png`)
       expect(res.status).toEqual(302)
       expect(res.text).toEqual(
-        `Redirecting to <a href="/builder/logo_supertoolmake.png">/builder/logo_supertoolmake.png</a>.`
+        `Redirecting to /builder/logo_supertoolmake.png.`
       )
     })
   })
