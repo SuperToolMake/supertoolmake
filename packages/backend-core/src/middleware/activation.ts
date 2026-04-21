9f4807daf76705ba502e60d1f08e44f1bf98fb18
mohagan9 <bournonville_deisel@aleeas.com>
2026-04-21 23:14:08 +0100

    fix types

--- packages/backend-core/src/middleware/activation.ts
+++ packages/backend-core/src/middleware/activation.ts
@@ -1,11 +1,21 @@
-import { ConfigType, type SettingsConfig } from "@supertoolmake/types"
-import type { Context, Middleware, Next } from "koa"
 import { getConfig } from "../configs"
+import { ConfigType, EndpointMatcher, SettingsConfig } from "@supertoolmake/types"
+import type { Next, Middleware, Context } from "koa"
+import { buildMatcherRegex, matches } from "./matchers"
 
-export function activeTenant(): Middleware {
-  return async (ctx: Context, next: Next) => {
+export function activeTenant(
+  noActivationPatterns: EndpointMatcher[] = []
+): Middleware {
+  const noActivationOptions = buildMatcherRegex(noActivationPatterns)
+
+  return async function (ctx: Context, next: Next) {
+    if (matches(ctx as any, noActivationOptions)) {
+      return next()
+    }
     try {
-      const settingsConfig = await getConfig<SettingsConfig>(ConfigType.SETTINGS)
+      const settingsConfig = await getConfig<SettingsConfig>(
+        ConfigType.SETTINGS
+      )
       if (settingsConfig?.config?.active === false) {
         // Treat inactive tenant as if it doesn't exist - return 404 or unauthorized
         ctx.status = 404
