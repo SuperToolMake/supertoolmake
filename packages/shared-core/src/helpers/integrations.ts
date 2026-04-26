2fe5f4f0240890cc6b573c32e7a15f77c43230fa
mohagan9 <bournonville_deisel@aleeas.com>
2026-04-26 15:52:34 +0100

    lint

--- packages/shared-core/src/helpers/integrations.ts
+++ packages/shared-core/src/helpers/integrations.ts
@@ -1,9 +1,6 @@
 import { SourceName } from "@supertoolmake/types"
 
-export function isSQL(datasource: {
-  source: SourceName,
-  isSQL?: boolean
-}): boolean {
+export function isSQL(datasource: { source: SourceName; isSQL?: boolean }): boolean {
   const SQL = [SourceName.POSTGRES, SourceName.SQL_SERVER, SourceName.MYSQL]
   return SQL.indexOf(datasource.source) !== -1 || datasource.isSQL === true
 }
