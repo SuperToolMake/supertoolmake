79e86eea7710ba28959548c4c4e36579943ad48c
mohagan9 <bournonville_deisel@aleeas.com>
2026-06-15 21:56:58 +0100

    Fix build

--- packages/bbui/src/Notification/NotificationDisplay.svelte
+++ packages/bbui/src/Notification/NotificationDisplay.svelte
@@ -2,13 +2,25 @@
 import "@spectrum-css/toast/dist/index-vars.css"
 import { fly } from "svelte/transition"
 import Portal from "svelte-portal"
-import { notifications } from "../Stores/notifications"
+import {
+  notifications,
+  type Notification as NotificationItem,
+} from "../Stores/notifications"
 import Notification from "./Notification.svelte"
+
+let items = $state<NotificationItem[]>([])
+
+$effect(() => {
+  const unsubscribe = notifications.subscribe(value => {
+    items = value
+  })
+  return unsubscribe
+})
 </script>
 
 <Portal target=".modal-container">
   <div class="notifications">
-    {#each $notifications as { type, icon, message, id, dismissable, action, actionMessage, wide } (id)}
+    {#each items as { type, icon, message, id, dismissable, action, actionMessage, wide } (id)}
       <div transition:fly={{ y: 30 }}>
         <Notification
           {type}
