f1945210bbb87d14c51d4b4471c1cdf8311206c8
Andrew Thompson <110921612+andz-bb@users.noreply.github.com>
2026-06-18 21:55:33 +0100

    Merge pull request #19013 from Budibase/mongo-fixes
    mongodb and firestore datasource fixes

--- packages/server/src/api/routes/datasource.ts
+++ packages/server/src/api/routes/datasource.ts
@@ -1,23 +1,21 @@
-import { permissions } from "@supertoolmake/backend-core"
-import { authorizedMiddleware as authorized } from "../../middleware/authorized"
 import * as datasourceController from "../controllers/datasource"
-import { builderRoutes, endpointGroupList } from "./endpointGroups"
 import { datasourceValidator } from "./utils/validators"
-
-const authorizedRoutes = endpointGroupList.group({
-  middleware: authorized(permissions.PermissionType.TABLE, permissions.PermissionLevel.READ),
-  first: false,
-})
+import { builderRoutes } from "./endpointGroups"
 
 builderRoutes
   .get("/api/datasources", datasourceController.fetch)
   .post("/api/datasources/verify", datasourceController.verify)
   .post("/api/datasources/info", datasourceController.information)
   .post("/api/datasources/views", datasourceController.viewInformation)
-  .post("/api/datasources/relationships", datasourceController.getRelationshipInformation)
-  .post("/api/datasources/:datasourceId/schema", datasourceController.buildSchemaFromSource)
+  .post(
+    "/api/datasources/relationships",
+    datasourceController.getRelationshipInformation
+  )
+  .post(
+    "/api/datasources/:datasourceId/schema",
+    datasourceController.buildSchemaFromSource
+  )
+  .get("/api/datasources/:datasourceId", datasourceController.find)
   .put("/api/datasources/:datasourceId", datasourceController.update)
   .post("/api/datasources", datasourceValidator(), datasourceController.save)
   .delete("/api/datasources/:datasourceId/:revId", datasourceController.destroy)
-
-authorizedRoutes.get("/api/datasources/:datasourceId", datasourceController.find)
