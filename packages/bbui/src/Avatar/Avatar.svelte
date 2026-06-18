d7819a666737e706bb8c6bd7b9be33b5980a0e2e
melohagan <101575380+melohagan@users.noreply.github.com>
2026-06-18 08:03:55 +0100

    Merge pull request #19017 from Budibase/ts/bbui-avatar
    Convert BBUI Avatar component to TypeScript

--- packages/bbui/src/Avatar/Avatar.svelte
+++ packages/bbui/src/Avatar/Avatar.svelte
@@ -1,40 +1,49 @@
-<script>
-import "@spectrum-css/avatar/dist/index-vars.css"
+<script lang="ts">
+  import "@spectrum-css/avatar/dist/index-vars.css"
 
-let sizes = new Map([
-  ["XXS", "--spectrum-alias-avatar-size-50"],
-  ["XS", "--spectrum-alias-avatar-size-75"],
-  ["S", "--spectrum-alias-avatar-size-200"],
-  ["M", "--spectrum-alias-avatar-size-400"],
-  ["L", "--spectrum-alias-avatar-size-500"],
-  ["XL", "--spectrum-alias-avatar-size-600"],
-  ["XXL", "--spectrum-alias-avatar-size-700"],
-])
-export let size = "M"
-export let url = ""
-export let disabled = false
-export let initials = "JD"
-export let color = ""
+  type AvatarSize = "XXS" | "XS" | "S" | "M" | "L" | "XL" | "XXL"
 
-const DefaultColor = "#3aab87"
+  let sizes = new Map<AvatarSize, string>([
+    ["XXS", "--spectrum-alias-avatar-size-50"],
+    ["XS", "--spectrum-alias-avatar-size-75"],
+    ["S", "--spectrum-alias-avatar-size-200"],
+    ["M", "--spectrum-alias-avatar-size-400"],
+    ["L", "--spectrum-alias-avatar-size-500"],
+    ["XL", "--spectrum-alias-avatar-size-600"],
+    ["XXL", "--spectrum-alias-avatar-size-700"],
+  ])
+  export let size: AvatarSize = "M"
+  export let url: string = ""
+  export let disabled: boolean = false
+  export let initials: string = "JD"
+  export let color: string = ""
 
-const getColor = (initials) => {
-  if (!initials?.length) {
-    return DefaultColor
+  const defaultColor = "#3aab87"
+
+  let avatarColor: string
+  let style: string
+
+  $: avatarColor = color || getColor(initials)
+  $: style = getStyle(size, avatarColor)
+
+  const getColor = (initials: string): string => {
+    if (!initials?.length) {
+      return defaultColor
+    }
+    const code = initials[0].toLowerCase().charCodeAt(0)
+    const hue = ((code % 26) / 26) * 360
+    return `hsl(${hue}, 50%, 50%)`
   }
-  const code = initials[0].toLowerCase().charCodeAt(0)
-  const hue = ((code % 26) / 26) * 360
-  return `hsl(${hue}, 50%, 50%)`
-}
 
-const getStyle = (sizeKey, color) => {
-  const size = `var(${sizes.get(sizeKey)})`
-  const fontSize = `calc(${size} / 2)`
-  return `width:${size}; height:${size}; font-size:${fontSize}; background:${color};`
-}
-
-$: avatarColor = color || getColor(initials)
-$: style = getStyle(size, avatarColor)
+  const getStyle = (sizeKey: AvatarSize, color: string): string => {
+    const sizeValue = sizes.get(sizeKey)
+    if (!sizeValue) {
+      return `background:${color};`
+    }
+    const size = `var(${sizeValue})`
+    const fontSize = `calc(${size} / 2)`
+    return `width:${size}; height:${size}; font-size:${fontSize}; background:${color};`
+  }
 </script>
 
 {#if url}
